36e1e1a15b14c7e4ed4e0468f2c68628dac25d7f
SuyashPatil-29 <112413583+SuyashPatil-29@users.noreply.github.com>
2023-07-05 22:27:32 +0530

    Started trello

---
+++ src/app/components/Board.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {DragDropContext, Droppable} from 'react-beautiful-dnd'
+
+const Board = () => {
+  return (
+    // <DragDropContext>
+    //     <Droppable droppableId='board' direction='horizontal' type='column'>
+    //         {(provided) => (
+    //             <div>A</div>
+    //         )}
+    //     </Droppable>
+    // </DragDropContext>
+    <h1>Drag And Drop</h1>
+  )
+}
+
+export default Board
